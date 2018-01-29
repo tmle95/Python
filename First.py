7b473bf74422e08aa026e157150f682c4dcf77e9
Tony Trong Le <34502464+tmle95@users.noreply.github.com>
2018-01-29 01:33:56 -0600

    simple program

--- First.py
+++ First.py
@@ -1,4 +1,4 @@
-"""Tony-Trong Le Assignment #1 COSC 1306 MW 2:30-4:00 Fall 2016 Computes guests shares of a bill"""
+Computes guests shares of a bill"""
 bill = float(input("Enter the total check amount: "))
 tiprate = float(input("Enter tip percentage: "))
 nguests = float(input("Enter the number of guets: "))
